bf43edad8a79c7306c52e868f7d9bbce64d1f4a6
HOVO97 <hsanosyan@bk.ru>
2020-07-08 23:06:46 -1200

    fourth commit

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -12,11 +12,14 @@
         <label for="model">Model</label>
         <input type="text" name="model" id="model" required>
         <label for="price">Prize</label>
-        <input type="number" name="price" id="price" required>
+        <input type="number" name="price" id="price" required><br>
+        <hr>
         <label for="content">Content</label>
-        <input type="text" name="cont" id="content" required>
-        <label for="img">Picture</label>
-        <input type="file" name="select_file" id="img" required>
+        <textarea style="width: 400px;height: 100px;resize: none;" type="text" name="cont" id="content"  required></textarea><br>
+        <hr>
+        <label for="img">Picture<img style="width: 80px;" src="{{ URL::asset("images/upload.png")}}" alt=""></label><br>
+        <hr>
+        <input style="display: none" type="file" name="select_file" id="img"   required>
         <input type="submit" value="Save" class="btn btn-success">
     </form>
 </div>
